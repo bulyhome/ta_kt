fcb3b0b210ac8742c82fc1ea4948bb7e5434a562
bulyhome <bu1yh0m3@yahoo.com>
2019-02-26 12:18:00 +0200

    Fixed ta_kt.py

--- ta_kt.py
+++ ta_kt.py
@@ -5,13 +5,8 @@ import re
 pkg_path = "PKG_contents_0"
 
 if __name__ == '__main__':
-    # result_list = ta.get_test_scenarios(pkg_path)
-    # final_result = 'OK' if ta.check_all_tests(result_list) else 'NOK'
-    # print(final_result)
+    result_list = ta.get_test_scenarios(pkg_path)
+    final_result = 'OK' if ta.check_all_tests(result_list) else 'NOK'
+    print(final_result)
 
-    filename = 'ZX_CC_free_text.test_scenario.xml'
-    match_obj = re.match(r'^([A-Z0-9]+)_(FC|BB|CC|SY|UT)_(.*).test_scenario.xml$', filename)
-    if match_obj:
-        print(match_obj.group(1))
-        print(match_obj.group(2))
-        print(match_obj.group(3))
+
